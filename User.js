76eb5ae540d9247a22e8eca16872fb94f1476a30
Aniketmulik <138675235+Aniketmulik@users.noreply.github.com>
2023-07-31 11:02:29 +0530

    Update User.js

--- User.js
+++ User.js
@@ -1,10 +1,11 @@
 const Game=require("./Game");
 
-let g1=Game.newGame("Dhruv","Susminder");
-let g2=Game.newGame("Nikunj","Aniket");
+let g1=Game.newGame("sid","ajay");
+let g2=Game.newGame("rohan","Aniket");
 
-console.log(g1.play(0))
-console.log(g1.play(3))
-console.log(g1.play(1))
+console.log(g1.play(8))
 console.log(g1.play(4))
-console.log(g1.play(2))
+console.log(g1.play(7))
+console.log(g1.play(1))
+console.log(g1.play(6))
+console.log(g1.play(5))
